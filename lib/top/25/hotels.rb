301ed5ad7de77e737504e16d6b27c62a5ca2d9db
Yardagan AL Hamed <gooryalhamed@hotmail.com>
2016-12-17 12:44:36 +0000

    initialize a git repo

---
+++ lib/top/25/hotels.rb
@@ -0,0 +1,9 @@
+require "top/25/hotels/version"
+
+module Top
+  module 25
+    module Hotels
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/top/25/hotels/version.rb
@@ -0,0 +1,7 @@
+module Top
+  module 25
+    module Hotels
+      VERSION = "0.1.0"
+    end
+  end
+end
