3a61ebb5e7547cc20c21fc7233246ed5fa9475eb
Yardagan AL Hamed <gooryalhamed@hotmail.com>
2017-01-23 16:47:11 +0000

    stage deleted files

--- lib/top/25/hotels.rb
+++
@@ -1,9 +0,0 @@
-require "top/25/hotels/version"
-
-module Top
-  module 25
-    module Hotels
-      # Your code goes here...
-    end
-  end
-end
--- lib/top/25/hotels/version.rb
+++
@@ -1,7 +0,0 @@
-module Top
-  module 25
-    module Hotels
-      VERSION = "0.1.0"
-    end
-  end
-end
