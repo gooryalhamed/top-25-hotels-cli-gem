c6f3bca91648c3609215de7fedf0e2100d18e1cc
Yardagan AL Hamed <gooryalhamed@hotmail.com>
2017-01-26 18:17:43 +0000

    add scrape_show_page

--- lib/top25/scraper.rb
+++ lib/top25/scraper.rb
@@ -1,5 +1,5 @@
 class Top25::Scraper
-  
+
   def scrap(placename)
     scrapped_places =[]
     html = open("https://www.tripadvisor.com/TravelersChoice-#{placename}")
@@ -20,6 +20,23 @@ class Top25::Scraper
       end
     end
     return scrapped_places
+  end
+def scrap_show_page(url)
+  html = open("#{url}")
+  scrapped_page = Nokogiri::HTML(html)
+  arr1 = scrapped_page.css(".bl_details")
+  arr2 = scrapped_page.css("div.property_tags")
+  show_h = {}  # {:phone => "", :website =>"", :address => {}, :description =>""}
+
+    show_h[:phone] = arr1.css("div.f1 .f1").text
+    show_h[:website_url] = arr1.css("div.f1").attribute("href").value
+
+    address = arr1.css("div.format_address")
+
+      show_h[:address][:street] = address.css("div.street_address").text
+      show_h[:address][:locality] = address.css("div.locality").text
+      show_h[:address][:country]= address.css("div.country_name").text
+      show_h
   end
 
 end
