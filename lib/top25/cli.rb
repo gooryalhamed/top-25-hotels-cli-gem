4b370e0c3c1fea551d492cdadd6cd15b176b6936
Yardagan AL Hamed <gooryalhamed@hotmail.com>
2017-01-29 13:27:18 +0000

    change the cli logic

--- lib/top25/cli.rb
+++ lib/top25/cli.rb
@@ -1,32 +1,34 @@
 class Top25::Cli
 
-  def search_for
-    search =""
-    puts "Search for the best in the world, choose on of the follwing numbers"
-    puts "1. Hotels\n2. Restaurants\n3. Beaches"
-    search = gets.strip
-    case search
+    def home
+      puts "Search for the best in the world, choose on of the follwing numbers"
+      puts "1. Hotels\n2. Restaurants\n3. Beaches\n4. e for exit\n5. b for be back"
+    end
+
+def search(s)
+  home
+  input =""
+  input = gets.strip
+  case s
     when "1"
-        run("Hotels")
-      when "2"
-        run("Restaurants")
-      when "3"
-        run("Beaches")
-      else
+      run("Hotels")
+    when "2"
+      run("Restaurants")
+    when "3"
+      run("Beaches")
+    when "b"
+      home
+    when s!= "1" || s!= "2" || s!="3" || s!="b"
         puts "sorry, you only can search hotels,restaurants and beaches"
-      end
-  end
+    when "e"
+      puts "Good bye!!"
+    end
+end
 
   def run(place)
     make_places(place)
     #add_attributes
     display_places
-    research = ""
-    puts "search_for or exit?"
-    research = gets.strip
-    unless research == "exit"
-      search_for
-    end
   end
 
   def make_places(name)
